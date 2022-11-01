a9e389cc82de72d1d647fbea017cc97f6c4f70c9
Opavoo <108014501+Opavoo@users.noreply.github.com>
2022-11-01 14:58:42 +0300

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,13 @@
+let myAge = 28;
+let AgeBestFriend = 28;
+let myBirthAge = 1994;
+let yearNow = 2022;
+let rubInDollar = 61.37;
+
+const myName = "Людмила";
+const NameBestFriend = "Полина";
+const myCity = "Санкт-Петербург";
+const capitalMyCountry = "Москва";
+const myDescription = "Девушка";
+
+console.log('Меня зовут ' + myName + ',' + ' мне ' + myAge + ' года/лет. Я из города ' + myCity + ' и я ' + myDescription + '.');
